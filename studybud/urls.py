58f00819783f60d5e30b032bfefc0ce5c0f7c232
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2022-06-22 22:08:44 +0200

    intial commit

--- studybud/urls.py
+++ studybud/urls.py
@@ -15,17 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Home Page')
-
-def room(request):
-    return HttpResponse("ROOM")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('room/', room)
 ]
